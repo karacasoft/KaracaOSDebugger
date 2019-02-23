bcfad3d0cde9183ebb297b98ea2d9387e069fe0e
Mahmut Karaca <mahmutkaraca95@gmail.com>
2019-02-23 19:10:36 +0300

    remove build dir and add it to .gitignore

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const socket = require("socket.io");
-const KaracaOSDebugger_1 = require("./debugger/KaracaOSDebugger");
-const io = socket(5000);
-io.on('connection', KaracaOSDebugger_1.serveOnSocketIo());
-//# sourceMappingURL=index.js.map
